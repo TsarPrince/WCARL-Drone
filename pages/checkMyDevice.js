4fcc468b8b8deb39f2ee2913f5a49346c9ab1b66
flyingdelta <flyingdelta@outlook.com>
2022-08-24 22:08:20 +0530

    display rounded values from the sensors

--- pages/checkMyDevice.js
+++ pages/checkMyDevice.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { checkSensors, checkMediaSupport } from "../public/scripts/sensorCheck";
+import { Round } from "../public/scripts/Utils";
 
 export default function CheckDevice() {
   let [cameraStat, setCameraStat] = useState(false);
@@ -12,16 +13,37 @@ export default function CheckDevice() {
       }
     });
     let results = checkSensors();
-    if (results.hasOwnProperty("gyroscope")) setgyroscopeStat(true);
-    if (results.hasOwnProperty("accelerometer")) setAccelerometerStat(true);
+    // Compatability -> https://developer.mozilla.org/en-US/docs/Web/API/Gyroscope#browser_compatibility
+    if (results.hasOwnProperty("gyroscope")) {
+      let gyroscope = results["gyroscope"];
+      gyroscope.addEventListener("reading", () => {
+        setgyroscopeStat(
+          `Angular Velocities => X: ${Round(gyroscope.x)}, Y: ${Round(
+            gyroscope.y
+          )}, Z: ${Round(gyroscope.z)}`
+        );
+      });
+    }
+    if (results.hasOwnProperty("accelerometer")) {
+      let acl = results["accelerometer"];
+      acl.addEventListener("reading", () => {
+        setAccelerometerStat(
+          `Acceleration => X: ${Round(acl.x)}, Y: ${Round(acl.y)}, Z: ${Round(
+            acl.z
+          )}`
+        );
+      });
+    }
   }, []);
   return (
     <>
       <h2>Your device supports: </h2>
       <ul>
         <li>Camera: {cameraStat ? "true" : "false"}</li>
-        <li>Accelerometer: {accelerometerStat ? "true" : "false"}</li>
-        <li>Gyroscope: {gyroscopeStat ? "true" : "false"}</li>
+        <li>
+          Accelerometer: {accelerometerStat ? accelerometerStat : "false"}
+        </li>
+        <li>Gyroscope: {gyroscopeStat ? gyroscopeStat : "false"}</li>
       </ul>
     </>
   );
